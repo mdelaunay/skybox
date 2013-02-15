07b547994f00ff257d3d5190860db64a24746ea9
mdelaunay <mederic.delaunay@gmail.com>
2013-02-16 00:23:02 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skybox::Application.config.secret_token = 'b511d2c926210b1933d6afa3826574318d12a156857053c1e61e19b8af7829f9cab59a346a74b7414e28331e7ea8344a1e5d8577dbe6dfeadf79a08e7c32c276'
